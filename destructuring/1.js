427eb3ff6769e42acd7cc0f89a12335a8edd851a
MohsinTaj <103893184+MohsinTaj@users.noreply.github.com>
2022-08-29 22:41:29 +0500

    changes

--- destructuring/1.js
+++ destructuring/1.js
@@ -44,3 +44,38 @@ show(language1);
 const last2=["a1","a2","a3","a4"]
 let[p,o,q]=last2
 console.log(p,o)
+//e.g:6
+const zz=["puppy","salvaster"]
+const [dogname,catname]=zz
+console.log(`the name of dog is ${dogname} and cat is ${catname}`)
+const dog={
+  name:'max',
+  age:5,
+  breed:'mountain'
+}
+const{name,age,breed}=dog
+console.log(name,age,breed)
+const cat={
+  nam:'jelly',
+  ag:4,
+  bre:'persian'
+}
+const{nam:na,...rest1}=cat
+console.log(rest1)
+console.log(na)
+const data=[
+  {id:1,n:"1st"},
+  {id:2,n:"2st"},
+  {id:3,n:"3st"},
+  {id:4,n:"4st"}
+]
+const  itemcut=data.splice(0,1)
+console.log(itemcut)
+let dn="rexi"
+let cn="utia"
+// dn=cn
+[cn,dn]=[dn,cn]
+console.log(cn)
+console.log(dn)
+
+
